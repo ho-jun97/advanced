f2f411a272364646b4e23150ba556409c26fabf6
ho-jun97 <hojun9707@naver.com>
2026-02-09 22:36:55 +0900

    feat: 로그 추적기 v1-적용

---
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,38 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.hellotrace.HelloTraceV1;
+import hello.advanced.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV1 {
+    private final HelloTraceV1 trace;
+
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try{
+            status = trace.begin("OrderRepository.request()");
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalArgumentException("예외 발생!");
+            }
+
+            sleep(1000); // 1초간 상품 저장하는 가정
+            trace.end(status);
+        } catch (Exception ex){
+            trace.exception(status, ex);
+            throw ex; // 예외를 꼭 다시 던져주어야 한다.
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
